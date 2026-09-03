778767c1230c3cba51308f1a341e200c7dca7ab8
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-09-03 18:32:29 +0200

    319. Connection Between Spring Boot and RabbitMQ

---
+++ src/test/java/net/javaguides/springboot/rabbitmq/SpringbootRabbitmqTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
